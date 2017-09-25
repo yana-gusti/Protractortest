cf0df034daa3fb90f8adff059829fc8b24f2280a
yanagusti <yana.gusti@gmail.com>
2017-09-25 15:30:17 +0300

    first

--- test/conf.js
+++ test/conf.js
@@ -3,18 +3,12 @@
 
 exports.config = {
     framework: 'jasmine2',
-    // onPrepare: function () {
-    //     var AllureReporter = require('jasmine-allure-reporter');
-    //     jasmine.getEnv().addReporter(new AllureReporter());
-    //     jasmine.getEnv().afterEach(function(done){
-    //         browser.takeScreenshot().then(function (png) {
-    //             allure.createAttachment('Screenshot', function () {
-    //                 return new Buffer(png, 'base64')
-    //             }, 'image/png')();
-    //             done();
-    //         })
-    //     });
-    // },
+    onPrepare: function() {
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+    },
 
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['test1.js'],
