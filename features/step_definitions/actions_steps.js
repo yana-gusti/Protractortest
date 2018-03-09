6fe8a4a4b8a8079ebc3558265c0ec9e8941507f2
yana-gusti <yana.gusti@gmail.com>
2018-03-09 11:29:12 +0200

    delete defineSupportCode

--- features/step_definitions/actions_steps.js
+++ features/step_definitions/actions_steps.js
@@ -1,22 +1,18 @@
-const {defineSupportCode} = require('cucumber');
+const {Given, When} = require('cucumber');
 const pageObjects = require('../../test-data/page-objects');
 
-defineSupportCode(({Given, When}) => {
-    Given(/^User prints comment "([^"]*)"$/, (comment) => {
-        return console.log(comment);
-    });
+Given(/^User prints comment "([^"]*)"$/, (comment) => {
+    return console.log(comment);
+});
 
-    When(/^User navigates to the Calculator page$/, () => {
-        return browser.get(pageObjects.calculatorPage.url);
-    });
+When(/^User navigates to the Calculator page$/, () => {
+    return browser.get(pageObjects.calculatorPage.url);
+});
 
-    When(/^User enters (\d+) in field "([^"]*)"$/, (number, model) => {
-        return element(by.model(model)).sendKeys(number);
-    });
+When(/^User enters (\d+) in field "([^"]*)"$/, (number, model) => {
+    return element(by.model(model)).sendKeys(number);
+});
 
-    When(/^User clicks Go Button "([^"]*)"$/, (id) => {
-        return element(by.id(id)).click();
-    });
-
-
+When(/^User clicks Go Button "([^"]*)"$/, (id) => {
+    return element(by.id(id)).click();
 });
--- features/step_definitions/verifications_steps.js
+++ features/step_definitions/verifications_steps.js
@@ -1,16 +1,15 @@
-const {defineSupportCode} = require('cucumber');
+const {Then} = require('cucumber');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-defineSupportCode(({Then}) => {
 
-    Then(/^Page title is equal to "([^"]*)"$/, (title) => {
-        return expect(browser.getTitle()).to.eventually.equal(title);
-    });
-
-    Then(/^History "([^"]*)" is equal to (\d+)$/, (repeater, count) => {
-        return expect(element.all(by.repeater(repeater)).count()).to.eventually.equal(count);
-    });
+Then(/^Page title is equal to "([^"]*)"$/, (title) => {
+    return expect(browser.getTitle()).to.eventually.equal(title);
 });
+
+Then(/^History "([^"]*)" is equal to (\d+)$/, (repeater, count) => {
+    return expect(element.all(by.repeater(repeater)).count()).to.eventually.equal(count);
+});
+
