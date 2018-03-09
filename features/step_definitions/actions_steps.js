f7cc8b160e9350d96da104820b1169ffcbe16ec7
yana-gusti <yana.gusti@gmail.com>
2018-03-09 14:02:11 +0200

    first

--- features/step_definitions/actions_steps.js
+++ features/step_definitions/actions_steps.js
@@ -1,18 +1,18 @@
 const {Given, When} = require('cucumber');
 const pageObjects = require('../../test-data/page-objects');
 
-Given(/^User prints comment "([^"]*)"$/, (comment) => {
-    return console.log(comment);
-});
+    Given(/^User prints comment "([^"]*)"$/, (comment) => {
+        return console.log(comment);
+    });
 
-When(/^User navigates to the Calculator page$/, () => {
-    return browser.get(pageObjects.calculatorPage.url);
-});
+    When(/^User navigates to the Calculator page$/, () => {
+        return browser.get(pageObjects.calculatorPage.url);
+    });
 
-When(/^User enters (\d+) in field "([^"]*)"$/, (number, model) => {
-    return element(by.model(model)).sendKeys(number);
-});
+    When(/^User enters (\d+) in field "([^"]*)"$/, (number, model) => {
+        return element(by.model(model)).sendKeys(number);
+    });
 
-When(/^User clicks Go Button "([^"]*)"$/, (id) => {
-    return element(by.id(id)).click();
-});
+    When(/^User clicks Go Button "([^"]*)"$/, (id) => {
+        return element(by.id(id)).click();
+    });
